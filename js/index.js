2fd75d77d471d883d99ae8cb4330aa9dd5a7e2da
Juliano <julianoaugusto.xp6@gmail.com>
2024-01-24 18:07:40 -0300

    alteration route index.js

--- js/index.js
+++ js/index.js
@@ -3,7 +3,7 @@ import { Router } from './router.js'
 const router = new Router()
 router.add('/', "/pages/home.html")
 router.add('/universe', "/pages/theUniverse.html")
-router.add('/exploration', "/pages/Exploration.html")
+router.add('/exploration', "/pages/exploration.html")
 router.add(404, "/pages/404.html")
 
 router.handle()
